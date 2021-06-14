f57eff46674f3264c361218643864b60306d87b1
eparedes <eparedes@regionsanmartin.gob.pe>
2021-06-13 23:21:43 -0500

    subida nuevo proyecto

---
+++ lib/pages/Inventario/InventarioPage.dart
@@ -0,0 +1,216 @@
+import 'package:appventas/pages/Inventario/NuevoProductoPage.dart';
+import 'package:flutter/material.dart';
+class InventarioPage extends StatelessWidget {
+  const InventarioPage({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Column(
+          children: [
+            Text('Nombre del negocio'),
+            Text('Propietario',style: TextStyle(fontSize: 13),)
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding:  EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    //height: 50,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Padding(
+                      padding:  EdgeInsets.only(top: 16,bottom: 16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Total de productos'),
+                          SizedBox(height: 8,),
+                          Text('25',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    //height: 50,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Padding(
+                      padding:  EdgeInsets.only(top: 16,bottom: 16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Costo invertido'),
+                          SizedBox(height: 8,),
+                          Text('S/100000.00',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 12,),
+
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.grey[200]
+                ),
+               child: Column(
+                 children: [
+                   Container(
+                     //height: 40,
+                     
+                     child: Padding(
+                       padding:  EdgeInsets.only(top: 8,right: 8,left: 8),
+                       child: Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: [
+                           Expanded(
+                             child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Buscar productos ..',
+                                  prefixIcon: Icon(Icons.search),
+                                  border: InputBorder.none
+                                ),
+                              ),
+                             ),
+                           ),
+                           Icon(Icons.download_rounded,size: 40,)
+                         ],
+                       ),
+                     ),
+                   ),
+                  SizedBox(height:12),
+
+                  Container(
+                    height: 55,
+                    
+                    //color: Colors.black,
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Colors.blueGrey,
+                          child: Center(
+                            child:Icon(Icons.edit,color:Colors.white)
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 8,
+                              itemBuilder: (context,i){
+                                return Padding(
+                                  padding:  EdgeInsets.only(left: 8,right:8),
+                                  child: Container(
+                                    height: 50,
+                                    width: 100,
+                                    color: Colors.blue,
+                                    child: Center(
+                                      child: Padding(
+                                        padding:  EdgeInsets.only(left: 8,right: 8),
+                                        child: Text(
+                                          "Categoria $i",
+                                          overflow: TextOverflow.ellipsis,
+                                          style:TextStyle(
+                                            color:Colors.white,
+                                          ) 
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+
+                              }
+                            ),
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                  SizedBox(height:12),
+                  Expanded(
+                    child: Container(
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: 4,
+                        itemBuilder: (context,i){
+                          return Card(
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    height: 90,
+                                    width: 90,
+                                    color: Colors.black,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text('nombre producto'),
+                                      Text('12 disponible'),
+                                      Text('S/120.00')
+                                    ],
+                                  )
+
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    ),
+                  )
+
+                 ],
+               ),
+              )
+            )
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (BuildContext context) => NuevoProductoPage()));
+          
+        },
+        child: Text('Crear producto'),
+      ),
+      
+    );
+  }
+}
---
+++ lib/pages/Inventario/NuevoProductoPage.dart
@@ -0,0 +1,269 @@
+import 'package:flutter/material.dart';
+
+class NuevoProductoPage extends StatelessWidget {
+  const NuevoProductoPage({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Agregar producto'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 10,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))
+                ),
+                child: Column(
+                    children: [
+                      SizedBox(height: 15,),
+                      Container(
+                        height: 140,
+                        width: double.infinity,
+                        //color: Colors.black,
+                        child: Padding(
+                          padding:  EdgeInsets.only(left: 12,right: 12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                height: 130,
+                                width: MediaQuery.of(context).size.width*1/2,
+                                decoration: BoxDecoration(
+                                  color: Colors.black54,
+                                  borderRadius: BorderRadius.circular(15)
+                                ),
+                                child: Center(
+                                  child: Icon(Icons.camera_alt,color: Colors.white,size: 60,),
+                                ),
+                              ),
+                              SizedBox(width: 12,),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text('Codigo de barras'),
+                                    Container(
+                                      height: 45,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.grey),
+                                        borderRadius: BorderRadius.circular(15)
+                                      ),
+                                      child: Center(
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          children: [
+                                            Text('Escanea aqui'),
+                                            Icon(Icons.qr_code_scanner_rounded)
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+
+                                    SizedBox(height: 12,),
+                                    Text('Cantidad disponible'),
+                                    Container(
+                                      height: 45,
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Expanded(
+                                            child: Container(
+                                              alignment: Alignment.center,
+                                              height: 40,
+                                              decoration: BoxDecoration(
+                                                border: Border.all()
+                                              ),
+                                              child: Center(child: Icon(Icons.minimize))
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Container(
+                                              height: 40,
+                                              decoration: BoxDecoration(
+                                                border: Border.all()
+                                              ),
+                                              child: Center(child: Text('2'))
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Container(
+                                              height: 40,
+                                              decoration: BoxDecoration(
+                                                border: Border.all()
+                                              ),
+                                              child: Center(child: Icon(Icons.add))
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    )
+
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView(
+                          children: [
+                            SizedBox(height: 30,),
+                            Padding(
+                                    padding:  EdgeInsets.only(left: 10,right: 10,top: 10),
+                                    child: Container(
+                                      height: 80,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.grey[300]
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,top: 8),
+                                            child: Text('Nombre'),
+                                          ),
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,right: 8),
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                border: InputBorder.none,
+                                                hintText: 'Producto (obligatorio)',
+                                                icon: Icon(Icons.description)
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                            Padding(
+                                    padding:  EdgeInsets.only(left: 10,right: 10,top: 10),
+                                    child: Container(
+                                      height: 80,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.grey[300]
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,top: 8),
+                                            child: Text('Precio unitario'),
+                                          ),
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,right: 8),
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                border: InputBorder.none,
+                                                hintText: 'Valor de venta (obligatorio)',
+                                                icon: Icon(Icons.local_atm)
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+
+                            Padding(
+                                    padding:  EdgeInsets.only(left: 10,right: 10,top: 10),
+                                    child: Container(
+                                      height: 80,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.grey[300]
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,top: 8),
+                                            child: Text('Costo unitario'),
+                                          ),
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,right: 8),
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                border: InputBorder.none,
+                                                hintText: 'Valor de compra (obligatorio)',
+                                                icon: Icon(Icons.local_atm)
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                            Padding(
+                                    padding:  EdgeInsets.only(left: 10,right: 10,top: 10),
+                                    child: Container(
+                                      height: 80,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.grey[300]
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,top: 8),
+                                            child: Text('Categoría'),
+                                          ),
+                                          Padding(
+                                            padding:  EdgeInsets.only(left: 8,right: 8),
+                                            child: Row(
+                                              children: [
+                                                Icon(Icons.arrow_drop_down,size: 50,),
+                                                SizedBox(width: 8,),
+                                                Expanded(
+                                                  child: TextField(
+                                                    readOnly: true,
+                                                    decoration: InputDecoration(
+                                                      border: InputBorder.none,
+                                                      hintText: 'Categoria',
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                          ],
+                        ),
+                      )
+                      
+                    ],
+                  ),
+                
+                 
+              ),
+            )
+
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
